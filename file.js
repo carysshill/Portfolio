d2dc91e7a8ac631f702f040a208d131c0658cc79
carysshill <caryss.hill@gmail.com>
2023-11-30 13:49:01 -0700

    javascript

---
+++ file.js
@@ -0,0 +1,38 @@
+console.log("Hello World")
+console.log(new Date().getFullYear())
+document.getElementById("footer").innerHTML = ("Free to use under the Unsplash License "+(new Date().getFullYear()))
+sayHello = () => {
+        alert("hey girl");
+}
+document.getElementById('Cute-Dogs').onmouseover = () => { 
+    document.getElementById('Cute-Dogs').innerText =  'boo';
+};
+document.getElementById('Cute-Dogs').onmouseleave = () => {
+    document.getElementById('Cute-Dogs').innerText = 'Cute Dogs';
+}
+ let count = 1;
+ document.getElementById('btn-counter').onclick = () => {
+    count = count + 1;
+    if (count % 2 === 0 ) {
+        document.getElementById('txt-counter').classList.add('even');
+        document.getElementById('txt-counter').classList.remove('odd');
+    } else {
+        document.getElementById('txt-counter').classList.add('odd');
+        document.getElementById('txt-counter').classList.remove('even');
+    }
+    document.getElementById('txt-counter').innerHTML = count
+}
+for (let i = 1; i <= 99; i = i + 1) {
+    const newLi = document.createElement('Li');
+    newLi.textContent = i;
+    document.getElementById('numbers').appendChild(newLi)
+    if (count % 2 === 0) {
+        document.getElementById('numbers').classList.add('even');
+        document.getElementById('numbers').classList.remove('odd');
+    } else {
+        document.getElementById('numbers').classList.add('odd');
+        document.getElementById('numbers').classList.remove('even');
+    }
+}
+
+
